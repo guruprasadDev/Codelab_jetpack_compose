903863f63832d77d3f8d365d92711c140e4a25ce
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-19 20:26:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/praticejetpackcompose/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.praticejetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.praticejetpackcompose.ui.theme.PraticeJetPackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PraticeJetPackComposeTheme {
+                setContent {
+                }
+            }
+        }
+    }
+}
